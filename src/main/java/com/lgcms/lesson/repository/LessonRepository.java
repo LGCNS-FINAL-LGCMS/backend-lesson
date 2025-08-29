03c2045fee88c6a092a971e4e08096fbe0f4d03c
하리보냠 <drppr99@gmail.com>
2025-08-29 11:18:03 +0900

    [LB-292] fix : 카프카 이미지 수정 및 강좌 조회 정렬 변경

--- src/main/java/com/lgcms/lesson/repository/LessonRepository.java
+++ src/main/java/com/lgcms/lesson/repository/LessonRepository.java
@@ -2,6 +2,8 @@ package com.lgcms.lesson.repository;
 
 import com.lgcms.lesson.domain.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
@@ -11,7 +13,15 @@ import java.util.List;
 public interface LessonRepository extends JpaRepository<Lesson, String> {
     List<Lesson> findAllByLectureId(String lectureId);
 
-    List<Lesson> findAllByLectureIdOrderById(String lectureId);
+    @Query("""
+        SELECT l
+        FROM Lesson l
+        WHERE l.lectureId = :lectureId
+        ORDER BY l.createdAt
+        """)
+    List<Lesson> findAllByLectureIdOrderByIdCUSTOM(@Param("lectureId") String lectureId);
+
+    List<Lesson> findAllByLectureIdOrderByCreatedAt(String lectureId);
 
     void deleteAllByLectureId(String lectureId);
 }
