4f1b2fc56be96d67c9c277ad17f43f00c2fd6f94
하리보냠 <drppr99@gmail.com>
2025-09-01 10:00:58 +0900

    hotfix : 강좌 진행도 테이블 구조 변경

--- src/main/java/com/lgcms/lesson/domain/LessonProgress.java
+++ src/main/java/com/lgcms/lesson/domain/LessonProgress.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter@Builder
+@Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LessonProgress {
@@ -30,6 +31,8 @@ public class LessonProgress {
 
     private Integer lastWatched;
 
+    private Integer percentage;
+
     private LocalDateTime updatedAt;
 
     @PrePersist
@@ -38,9 +41,20 @@ public class LessonProgress {
         this.updatedAt = LocalDateTime.now();
     }
 
-    public void updatePlayTime(int playtime){
-        if(this.playtime < playtime) this.playtime = playtime;
+    @PrePersist
+    public void initProgress() {
+        this.playtime = 0;
+        this.lastWatched = 0;
+    }
+
+    public void updatePlayTime(int playtime) {
+        if (this.playtime < playtime) this.playtime = playtime;
         this.lastWatched = playtime;
+
+        double ratio = (double) this.playtime / this.totalPlaytime * 100;
+        int percent = (int) Math.round(ratio);
+
+        this.percentage = (percent >= 97) ? 100 : percent;
     }
 
 }
